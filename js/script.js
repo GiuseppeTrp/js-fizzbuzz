0741fc27a9ec0d7ae644dd85283065afbf796f12
GiuseppeTrp <trapassogiuseppe158@gmail.com>
2024-01-31 17:25:04 +0100

    end

--- js/script.js
+++ js/script.js
@@ -10,7 +10,7 @@ for(let i = 1; i <= 100 ; i++) {
     const newElement = document.createElement("div", "span");
     
     // creo delle " sottoclassi" per dare uno stile ai nuovi elementi inseriti
-    newElement.className = "square border rounded";
+    newElement.className = "square  border-0 rounded ";
     newElement.innerHTML = i;
     newElement.style.color = "black";
     newElement.style.fontWeight = "bold";
@@ -18,16 +18,16 @@ for(let i = 1; i <= 100 ; i++) {
 
     // faccio il controllo se la i è multipla solo di 3, solo di 5 o di entrambi i numeri
     if(i % 3== 0 && i % 5 == 0 ) {
-        newElement.style.background = "red"
+        newElement.style.background = "#1389B3"
         newElement.innerHTML = " FizzBuzz";   
     } else if(i % 5 == 0) {
-        newElement.style.background = "yellow"
+        newElement.style.background = "#FDCF65"
         newElement.innerHTML = " Buzz";   
     }else if(i % 3== 0 ) {
-        newElement.style.background = "lime"
+        newElement.style.background = "#0CD6A1"
         newElement.innerHTML = " Fizz";}
         else {
-            newElement.style.background = "blue"
+            newElement.style.background = "#1389B3"
             newElement.innerHTML = i;
         }  
 
